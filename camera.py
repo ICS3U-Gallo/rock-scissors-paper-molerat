9c4a155c565451e2abfd19a044e3c3d369a3bb6b
unknown <frankfrankfrankyeyeye@gmail.com>
2019-09-27 09:23:45 -0400

    Added scissors

--- camera.py
+++ camera.py
@@ -7,6 +7,7 @@ def main():
 
     fist_cascade = cv2.CascadeClassifier('fist.xml')
     palm_cascade = cv2.CascadeClassifier('palm_v4.xml')
+    fing_cascade = cv2.CascadeClassifier('finger.xml')
     # This haar-cascade was taken from https://github.com/Balaje/OpenCV/blob/master/haarcascades/fist.xml.
     # We may need to generate our own for paper and scissors.
     tolerance_level = []  # This is a queue
@@ -17,23 +18,27 @@ def main():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         palm_contours = palm_cascade.detectMultiScale3(gray, scaleFactor=1.2, minNeighbors=5, outputRejectLevels=True)
         fist_contours = fist_cascade.detectMultiScale3(gray, scaleFactor=1.2, minNeighbors=5, outputRejectLevels=True)
+        fing_contours = fing_cascade.detectMultiScale3(gray, scaleFactor=1.2, minNeighbors=5, outputRejectLevels=True)
         print(palm_contours)
         print(fist_contours)
         cv2.imshow('Camera', gray)
-        if type(palm_contours[2] != tuple):
-            try:  # HACK
-                if palm_contours[2][0] >= 1:
-                    tolerance_level.append('P')
-            except IndexError:
-                pass
-        if type(fist_contours[2] != tuple):
-            try:
-                if fist_contours[2][0] >= 2:
-                    tolerance_level.append('R')
-            except IndexError:
-                pass
+        try:  # HACK
+            if palm_contours[2][0] >= 1:
+                tolerance_level.append('P')
+        except IndexError:
+            pass
+        try:
+            if fist_contours[2][0] >= 2:
+                tolerance_level.append('R')
+        except IndexError:
+            pass
+        try:
+            if fing_contours[2][0] >= 2.1:
+                tolerance_level.append('S')
+        except IndexError:
+            pass
         if len(tolerance_level) >= 6:
-            tolerance_level = tolerance_level[:6]
+            tolerance_level = tolerance_level[1:7]
         if len(tolerance_level) == 5 and len(set(tolerance_level)) == 1:
             cap.release()
             cv2.destroyAllWindows()
