82891a36bc70b50c6b59b04515a41786248bf8d8
unknown <frankfrankfrankyeyeye@gmail.com>
2019-10-01 03:12:15 -0400

    More PEP8, merged Mark's weighted probability, and fixed it

--- my_main.py
+++ my_main.py
@@ -3,25 +3,9 @@ import camera
 import sys
 from PySide2.QtCore import QObject, Signal, Slot
 from PySide2.QtWidgets import *
-
-games = 0
-
-
-def good_games():
-    global games
-    games = int(input("Please say how many games you would like to play: "))
-
-    if games == 0:
-        print("Ok, see you later")
-        return
-
-    if games < 0:
-        raise ValueError("Try typing that again.")
-        good_games()
-
-    else:
-        raise ValueError("Try typing that again.")
-        good_games()
+rock_choices = 0
+paper_choices = 0
+scissors_choices = 0
 
 
 def get_full_move(letter: str) -> str:
@@ -31,9 +15,9 @@ def get_full_move(letter: str) -> str:
 def get_match_winner(human: str, computer: str) -> bool:
     """Determine the winner of a match of RPS.
     Returns -1 if the human won, 0 if tie, and 1 if computer won."""
-
-    global games
-    games = games - 1
+    global rock_choices
+    global scissors_choices
+    global paper_choices
 
     if human not in ["R", "P", "S"] or computer not in ["R", "P", "S"]:
         raise ValueError("Inputs must be either 'R', 'P', or 'S'!")
@@ -42,16 +26,19 @@ def get_match_winner(human: str, computer: str) -> bool:
         return 0
 
     if human == "R":
+        rock_choices += 1
         if computer == "S":
             return -1
         return 1
 
     if human == "P":
+        paper_choices += 1
         if computer == "R":
             return -1
         return 1
 
     if human == "S":
+        scissors_choices += 1
         if computer == "P":
             return -1
         return 1
@@ -60,7 +47,19 @@ def get_match_winner(human: str, computer: str) -> bool:
 def get_computer_move() -> str:
     """Generates a move to be played by the computer."""
 
-    return random.choice(["R", "P", "S"])
+    if rock_choices + paper_choices + scissors_choices > 10:
+        ceiling = rock_choices + paper_choices + scissors_choices + 1
+        weighted_choice = random.randint(1, ceiling)
+        if weighted_choice <= rock_choices:
+            return "R"
+        elif (weighted_choice > rock_choices and
+                weighted_choice <= paper_choices + rock_choices):
+            return "P"
+        elif weighted_choice > paper_choices+rock_choices:
+            return "S"
+
+    else:
+        return random.choice(["R", "P", "S"])
 
 
 class Form(QDialog):
@@ -94,7 +93,7 @@ class Form(QDialog):
         result = {1: "LOSE", 0: "TIE", -1: "WIN"}[winner]
         self.win_lose_text.setText(
             f"You played {get_full_move(human_move).upper()} "
-            f"and the computer played {get_full_move(computer_move).upper()}"
+            f"and the computer played {get_full_move(computer_move).upper()} "
             f"which means you {result}!\n\n"
             f"Computer wins: {self.computer_wins}\n"
             f"Human wins: {self.human_wins}\n"
